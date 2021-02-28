4d8e3b3a3f0e1f2cd6e982831b4820096468d27d
XoneKone <mr.blackmarch@mail.ru>
2021-03-01 00:32:05 +0300

    7ая задача
    Первый метод

--- 7th.rb
+++ 7th.rb
@@ -1,6 +1,24 @@
 def sum_noprime_div number
-	
+	sum = 0
+	if number > 3 then
+		number.downto(3) do |n|
+			if is_prime(n) == false && number % n == 0 then
+				sum += n
+			end
+		end
+	end
+	return sum
 end
+
+def is_prime number
+	(number-1).downto(2) do |n|
+		if number.gcd(n) != 1 then
+			return false
+		end
+	end 
+	return true
+end
+
 
 def count_less_three number
 
