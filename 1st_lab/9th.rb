ff77b9fe652e33293be2ec568f7b23dadfb06421
XoneKone <mr.blackmarch@mail.ru>
2021-03-08 19:55:27 +0300

    10ая задача
    problem 21

--- 1st_lab/9th.rb
+++ 1st_lab/9th.rb
@@ -71,3 +71,21 @@ def main
 		end	
 	end
 end
+
+=begin
+	Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
+	If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable numbers.
+	For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+
+	Evaluate the sum of all the amicable numbers under 10000.
+=end 
+
+def d x
+	(1...x).inject(0) {|mem,var| x % var == 0 ? mem + var : mem}
+end
+
+def problem21
+	p(((1..10000).select{|el| d(d(el)) == el && el != d(el)}).inject(0,:+))
+end
+
+problem21
