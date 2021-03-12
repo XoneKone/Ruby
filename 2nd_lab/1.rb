e547fc31a93be37b67d7736d95fbc039256ef378
XoneKone <mr.blackmarch@mail.ru>
2021-03-12 10:04:10 +0300

    2ая задача
    переписанные методы

--- 2nd_lab/1.rb
+++ 2nd_lab/1.rb
@@ -9,39 +9,19 @@ def first_method
 end
 
 def min list
-  min_val= 200000
-  list.each do |var|
-    if var < min_val
-      min_val = var
-    end
-  end
-  return min_val
+  list.min
 end
 
 def max list
-  max_val= -200000
-  list.each do |var|
-    if var > max_val
-      max_val = var
-    end
-  end
-  return max_val
+ list.max
 end
 
 def sum list
-  sum_val= 0
-  list.each do |var|
-    sum_val += var
-  end
-  return sum_val
+  list.inject(0,:+)
 end
 
 def prod list
-  prod_val= 1
-  list.each do |var|
-    prod_val *= var
-  end
-  return prod_val
+  list.inject(1,:*)
 end
 
 puts first_method
