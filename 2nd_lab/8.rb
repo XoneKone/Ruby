42c6c4e6ec23341a865c3bf39f4365c5438cb31a
XoneKone <mr.blackmarch@mail.ru>
2021-03-14 15:48:58 +0300

    9ая задача
    18 номер

--- 2nd_lab/8.rb
+++ 2nd_lab/8.rb
@@ -11,13 +11,17 @@ def task_6 str
 end
 
 def task_12 str
-	new_str = str.gsub(/[^\d]/,'') + str.gsub(/[\d]/,'') 
+	new_str = str.gsub(/[^\d]/,'') + str.gsub(/[\d]/,'')
+end
+
+def task_18 str
+	new_str = (str.scan(/([0-2]\d|3[0-1]).(0[1-9]|1[0-2]).(\d{4})/).map {|e| e.join('.')})
 end
 
 def main
 	str = read_from_file "text.txt"
 	str1 = "sdakjflj1232lkfsjdljf9023u4jfkjs0u13"
-	p  task_12 str1
+	p  task_18 "14.10.2000sdafasd213.324.23asdee12.12.2001"
 end 
 
 main
