a73f1df119a9ca5aced2c0ecc63ba60703536619
XoneKone <mr.blackmarch@mail.ru>
2021-03-14 16:24:17 +0300

    11ая задача
    6 номер

--- 2nd_lab/10.rb
+++ 2nd_lab/10.rb
@@ -7,10 +7,13 @@ def task_10 str
 	new_str = (str.scan(/([0-2]\d|3[0-1]) (января|февраля|марта|апреля|мая|июня|июля|августа|сентября|октября|ноября|декабря) (\d{4})/).map {|e| e.join(' ')})
 end
 
+def task_6 str
+	new_str = str.scan(/\d{1,}/).count {|e| e.to_i < 5}
+end
 
 def main
 	str = read_from_file 'date.txt'
-	puts task_10 str
+	puts task_6 str
 
 end
 
