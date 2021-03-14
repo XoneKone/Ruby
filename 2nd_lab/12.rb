eb9c2ef1775444af50a677f8d505e17f8dbff763
XoneKone <mr.blackmarch@mail.ru>
2021-03-14 19:24:02 +0300

    15
    3nomer

--- 2nd_lab/12.rb
+++ 2nd_lab/12.rb
@@ -16,9 +16,32 @@ def task14 lines
 	lines.sort_by { |e| e.split.index {|e| e =~ /[\d]/} != nil ? e.split.length - e.split.index {|e| e =~ /[\d]/} - 1  : 0}
 end
 
+def freq str
+	count_list = (str.split.map { |e| str.count(e) })
+	most_often = count_list.index(count_list.max)
+	str.count(str[most_often]).to_f / str.length
+end
+
+def task3 lines
+	lines.sort_by { |e| freq e}
+end
+
+def task6 lines
+	
+end
+
+def task9 lines
+end
+
+def task10 lines
+end
+
+
 def main
 	lines = read_from_file "lines.txt"	
-	p task14 lines	
+	p lines[4]
+	p  freq lines[4]
+	p task3 lines	
 end
 
 
