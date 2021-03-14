e4f8fc9262dbefeb7a58dc38679c45c25da09d84
XoneKone <mr.blackmarch@mail.ru>
2021-03-14 13:07:17 +0300

    9ая задача
    6ой номер

--- 2nd_lab/8.rb
+++ 2nd_lab/8.rb
@@ -6,9 +6,13 @@ def task8 str
 	(str.map { |e| e.to_i }).max
 end
 
+def task_6 str
+	str.map { |e| e[0] + e[1...-1].split('').shuffle.join + e[-1]}
+end
+
 def main
-	str = read_from_file "input.txt"
-	puts  task8 str
+	str = read_from_file "text.txt"
+	p  task_6 str
 end 
 
 main
