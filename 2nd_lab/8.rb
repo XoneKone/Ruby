f41221f87d5a3fdb935bcde00251c6a401ef8959
XoneKone <mr.blackmarch@mail.ru>
2021-03-14 13:19:48 +0300

    9ая задача
    12 номер

--- 2nd_lab/8.rb
+++ 2nd_lab/8.rb
@@ -10,9 +10,14 @@ def task_6 str
 	str.map { |e| e[0] + e[1...-1].split('').shuffle.join + e[-1]}
 end
 
+def task_12 str
+	new_str = str.gsub(/[^\d]/,'') + str.gsub(/[\d]/,'') 
+end
+
 def main
 	str = read_from_file "text.txt"
-	p  task_6 str
+	str1 = "sdakjflj1232lkfsjdljf9023u4jfkjs0u13"
+	p  task_12 str1
 end 
 
 main
