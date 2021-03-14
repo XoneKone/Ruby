691abef78bf31f72f0d81d9abec73c1aae07179f
XoneKone <mr.blackmarch@mail.ru>
2021-03-14 20:10:17 +0300

    15
    main

--- 2nd_lab/12.rb
+++ 2nd_lab/12.rb
@@ -13,7 +13,7 @@ def task13 lines
 end
 
 def task14 lines
-	lines.sort_by { |e| e.split.index {|e| e =~ /[\d]/} != nil ? e.split.length - e.split.index {|e| e =~ /[\d]/} - 1  : 0}
+	lines.sort_by { |e| e.split.index {|e| e =~ /[\d]/} != nil ? e.split.length - e.split.rindex {|e| e =~ /[\d]/} - 1  : 0}
 end
 
 def freq str
@@ -26,6 +26,12 @@ def palindrom str
 	(str.split.select { |e| e == e.reverse && e.length == 3}).length.to_f / str.split.length
 end
 
+def asci_codes str
+	codes = []
+	str.gsub(/ /,'').each_codepoint { |e| codes << e}
+	return codes
+end
+
 def task3 lines
 	lines.sort_by { |e| freq e}
 end
@@ -35,6 +41,7 @@ def task6 lines
 end
 
 def task9 lines
+
 end
 
 def task10 lines
@@ -44,8 +51,27 @@ end
 
 def main
 	lines = read_from_file "lines.txt"	
-	p lines[4]
-	p  task10 lines
+	puts "Исходный текст: "
+	p lines
+	puts "1. 3-ая задача\n2. 10-ая задача\n3. 12-ая задача\n4. 13-ая задача\n5. 14-ая задача"
+	print "Выберите задачу: "
+
+	answer = gets.chomp.to_i
+	print "Ответ: "
+	case answer
+	when 1
+			puts (task3 lines).to_s
+	when 2
+		puts (task10 lines).to_s
+	when 3
+		puts (task12 lines).to_s
+	when 4
+		puts (task13 lines).to_s
+	when 5
+		puts (task14 lines).to_s
+	else
+		puts "Нет такой задачи"
+	end
 end
 
 
